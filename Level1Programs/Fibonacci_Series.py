162875292bced11cd9aafb4ca4fef1148bed335d
tanushreepatil17 <tanushreepatil17@gmail.com>
2021-04-25 16:23:32 +0530

    Fibonacci_Series.py

--- Level1Programs/Fibonacci_Series.py
+++ Level1Programs/Fibonacci_Series.py
@@ -2,9 +2,9 @@ t1=0
 t2=1
 t3 = t1+t2
 n = int(input("Enter the number of terms you want to print: "))
-print(t1,t2, end=" ")
-for n in range(n):
+print(t1, end=" ")
+for n in range(n - 1):
     t1 = t2
     t2 = t3
     t3 = t1 + t2
-    print(t3,end=" ")
+    print(t2,end=" ")
