ac5a517099bd6879a668cee9e000c7d848f70374
tanushreepatil17 <tanushreepatil17@gmail.com>
2021-05-03 23:24:33 +0530

    Factorial.py

--- Level1Programs/Factorial.py
+++ Level1Programs/Factorial.py
@@ -1,9 +1,5 @@
 n = int(input("Enter the number : "))
-
-def fact(n):
-    x = 1
-    for i in range(1,n+1):
-        x = x * i
-    return x
-
-print(fact(n))
+x = 1
+for i in range(1,n+1):
+    x = x * i
+print(x)
